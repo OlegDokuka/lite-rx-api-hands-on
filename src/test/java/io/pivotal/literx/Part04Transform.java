2e474adb42410194a735485734789c52d0c56d57
Oleh Dokuka <shadowgun@i.ua>
2017-03-09 18:09:58 +0200

    implementation of examples parts

--- src/test/java/io/pivotal/literx/Part04Transform.java
+++ src/test/java/io/pivotal/literx/Part04Transform.java
@@ -29,7 +29,7 @@ public class Part04Transform {
 
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(u->new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
